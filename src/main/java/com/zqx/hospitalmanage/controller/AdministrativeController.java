581928e7d89ed7a3e474b3efb422086e171133a6
wangshu <wangshu464113010@outllok.com>
2019-03-22 10:22:03 +0800

    2018-03-21

--- src/main/java/com/zqx/hospitalmanage/controller/AdministrativeController.java
+++ src/main/java/com/zqx/hospitalmanage/controller/AdministrativeController.java
@@ -19,6 +19,7 @@ import java.util.List;
 @Controller
 @RequestMapping("administrative")
 public class AdministrativeController {
+
     @Autowired
     private AdministrativeService administrativeService;
 
