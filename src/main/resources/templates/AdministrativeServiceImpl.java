44672e56fd9648a366f34d09a6f270fa182ad1ce
zhangqian <2251403354@qq.com>
2019-04-05 10:18:19 +0800

    2019-04-05 上午 commit zq

---
+++ src/main/resources/templates/AdministrativeServiceImpl.java
@@ -0,0 +1,49 @@
+package com.zqx.hospitalmanage.service.impl;
+
+import com.zqx.hospitalmanage.dao.AdministrativeDao;
+import com.zqx.hospitalmanage.pojo.Administrative;
+import com.zqx.hospitalmanage.service.AdministrativeService;
+import com.zqx.hospitalmanage.util.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * 服务层
+ * @author Administrator
+ */
+@Service
+public class AdministrativeServiceImpl implements AdministrativeService {
+
+	@Autowired
+	private AdministrativeDao administrativeDao;
+
+	public List<Administrative> findAll() {
+		return administrativeDao.findAll();
+	}
+
+	public Administrative findOne(String id) {
+		return administrativeDao.findById(id).get();
+	}
+	@Transactional
+	public void update(Administrative administrative) {
+		administrativeDao.save(administrative);
+	}
+
+	@Override
+	@Transactional
+	public void save(Administrative administrative) {
+		administrative.setId(Utils.getUUID());
+		administrativeDao.save(administrative);
+	}
+	@Transactional
+	public void delete(String id) {
+		administrativeDao.deleteById(id);
+	}
+	@Transactional
+	public void deleteList(String[] ids) {
+	}
+
+}
---
+++ src/main/resources/templates/SolvePatientServiceImpl.java
@@ -0,0 +1,35 @@
+package com.zqx.hospitalmanage.service.impl;
+
+import com.zqx.hospitalmanage.dao.SolvePatientDao;
+import com.zqx.hospitalmanage.pojo.SolvePatient;
+import com.zqx.hospitalmanage.service.SolvePatientService;
+import com.zqx.hospitalmanage.util.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @version 1.0
+ * @program: hospitalmanage.SolvePatientServiceImpl
+ * @description:
+ * @author:
+ * @create: 2019-04-03 20:59:25
+ **/
+@Service
+public class SolvePatientServiceImpl implements SolvePatientService {
+
+    @Autowired
+    private SolvePatientDao solvePatientDao;
+
+    @Override
+    public List<SolvePatient> findAllByProblem(String problemId) {
+        return solvePatientDao.findAllByProblemId(problemId);
+    }
+    @Transactional
+    public void add(SolvePatient solvePatient){
+        solvePatient.setId(Utils.getUUID());
+        solvePatientDao.save(solvePatient);
+    }
+}
